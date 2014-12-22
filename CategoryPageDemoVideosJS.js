811d2bd3783f5f648a56d704549acbd2ea4d555e
Andre <andre.bulatov@gmail.com>
2014-12-22 17:49:43 -0500

    Added most up-to-date JS

---
+++ CategoryPageDemoVideosJS.js
@@ -0,0 +1,45 @@
+$(".Options").each(function checkForVideo(url) {
+
+	var ProductCatOpt = $(this);
+		ProductId = $(this).parent().parent().find('div.ProductImage').attr('data-product');
+
+	function ajax1() {
+	    return $.ajax('/content/videos/'+ProductId+'.mp4')
+		    .done(function() { 
+		        $(ProductCatOpt).addClass('withVideo');
+		    }).fail(function() { 
+		    	return;
+		    });
+		}
+		$.when(ajax1()).done(function(a1){
+
+	        $('.withVideo').closest('li').append('<span class="videoDemoBtn"><div class="triangle"></div></span>');
+	
+	        $('.videoDemoBtn').click(function() {    
+	
+	        if($(this).hasClass('videoPlaying')) {
+	            $(this).removeClass('videoPlaying');
+	            $(this).parent().find('img').show();
+	            $(this).parent().find('div.categoryDemoVideo').hide().html('');
+	            }
+	            else {
+	        
+	                var ProductId = $(this).parent().find('div.ProductImage').attr('data-product');
+	                $(this).addClass('videoPlaying');
+	                $(this).parent().find('img').hide();        
+	                $(this).parent().find('div.categoryDemoVideo').show().html('<video id="demoVideo" class="video" preload="auto" autoplay="autoplay" loop="loop" autobuffer="autobuffer" muted="muted" controls="controls" width="100%" height="100%"><source src="https://store-mixi7d.mybigcommerce.com/content/videos/'+ProductId+'.mp4"><source src="https://store-mixi7d.mybigcommerce.com/content/videos/'+ProductId+'.ogv" type="video/ogg"><p>Your browser does not support this video.  Please upgrade your browser!</p></video>');
+	                        }
+	                });
+
+            });
+
+
+/*
+	var video = document.getElementById('demoVideo');
+	video.addEventListener('click',function(){
+	  video.play();
+	},false);
+*/
+
+
+        });
