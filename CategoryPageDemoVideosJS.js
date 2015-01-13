fa6ccb972690a4ebc59b496dbc90a6a10ee80f97
Andre <andre.bulatov@gmail.com>
2015-01-13 00:20:14 -0500

    Update CategoryPageDemoVideosJS.js

--- CategoryPageDemoVideosJS.js
+++ CategoryPageDemoVideosJS.js
@@ -1,3 +1,20 @@
+	$(".Options").each(function checkForVideo(url) {
+		var ProductCatOpt = $(this);
+			ProductId = $(this).parent().parent().find('div.ProductImage').attr('data-product');
+		function ajax1() {
+		    return $.ajax('/content/Videos/'+ProductId+'.mp4')
+			    .done(function() { 
+			        ProductCatOpt.addClass('withVideo');
+			    }).fail(function() { 
+			    	return;
+			    });
+			}
+			$.when(ajax1()).done(function(a1){
+		        ProductCatOpt.closest('li').append('<span class="videoDemoBtn"><div class="triangle"></div></span>');
+	        });
+	    });
+
+
 	$('.ProductList').on('click', '.videoDemoBtn', function () {
 	        if ($(this).hasClass('videoPlaying')) {
 	            $(this).removeClass('videoPlaying');
@@ -9,9 +26,9 @@
 	                $(this).addClass('videoPlaying');
 /* 	                $(this).parent().find('img').hide();         */
 	                $(this).parent().find('div.categoryDemoVideo').show().html(
-	                	'<video id="demoVideo" class="video" preload="auto" autoplay="autoplay" loop="loop" autobuffer="autobuffer" muted="muted" controls="controls" width="100%" height="100%" poster="/content/videos/'+ProductId+'.jpg">'+
-	                		'<source src="/content/videos/'+ProductId+'.mp4">'+
-	                		'<source src="/content/videos/'+ProductId+'.ogv" type="video/ogg">'+
+	                	'<video id="demoVideo" class="video" preload autoplay loop autobuffer controls muted width="100%" height="100%" poster="/content/Videos/'+ProductId+'.jpg">'+
+	                		'<source src="/content/Videos/'+ProductId+'.mp4">'+
+	                		'<source src="/content/Videos/'+ProductId+'.ogv" type="video/ogg">'+
 	                		'<p>Your browser does not support this video.  Please upgrade your browser!</p>'+
 	                	'</video>');
 	                        }
@@ -22,19 +39,3 @@
 							},false);
 */
 	                });
-
-	$(".Options").each(function checkForVideo(url) {
-		var ProductCatOpt = $(this);
-			ProductId = $(this).parent().parent().find('div.ProductImage').attr('data-product');
-		function ajax1() {
-		    return $.ajax('/content/videos/'+ProductId+'.mp4')
-			    .done(function() { 
-			        ProductCatOpt.addClass('withVideo');
-			    }).fail(function() { 
-			    	return;
-			    });
-			}
-			$.when(ajax1()).done(function(a1){
-		        ProductCatOpt.closest('li').append('<span class="videoDemoBtn"><div class="triangle"></div></span>');
-	        });
-	    });
